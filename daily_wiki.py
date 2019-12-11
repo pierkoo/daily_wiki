59797801e30683431b5e9e30605ff83020776041
pierkoo <55929283+pierkoo@users.noreply.github.com>
2019-12-11 23:28:38 +0100

    storing articles/subscribers in database added

--- daily_wiki.py
+++ daily_wiki.py
@@ -1,10 +1,26 @@
 import requests
-
-r = requests.get('https://pl.wikipedia.org/wiki/Specjalna:Losowa_strona')
-
-with open('log.txt', 'w', encoding="utf-8") as open_file:
-    open_file.write(r.text)
+import database_management as dbm
+import urllib.parse
 
 
+r=requests.get('https://pl.wikipedia.org/wiki/Specjalna:Losowa_strona')
 
-print(r.url)
+
+
+# with open('log.txt', 'w', encoding="utf-8") as open_file:
+#     open_file.write(r.text)
+
+    title=urllib.parse.unquote(r.url.split('/')[-1]).replace('_',' ')
+    article=[title,r.url]
+
+    database = r"daily_wiki_database.db"
+
+    with dbm.create_connection(database) as comm:
+
+        try:
+            dbm.add_article(comm,article)
+        except dbm.sqlite3.IntegrityError:
+            print('Article already in the database!')
+
+        dbm.show_all_articles(comm)
+
--- yag_test.py
+++ yag_test.py
@@ -1,51 +1,22 @@
 import yagmail
-from email.mime.base import MIMEBase
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
-from datetime import date
+from datetime import datetime
 
-today=date.today()
-today=today.strftime("%d.%m.%Y")
+today=str(datetime.now())
+#today=today.strftime("%d.%m.%Y")
 
-receiver_email = 'pierkoo@gmail.com'
-
-message = MIMEMultipart("alternative")
-message["subject"] = "2Daily wikipedia for "+today
-message["From"] = 'Daily Wikipedia'
-message["To"] = receiver_email.split('@')[0]
+receiver_email = {'pierkoo@gmail.com':'pierkoo'}
 
 
-html = """\
-        <html>
-            <body>
-                <p>Hi!<br>
-                    How are you? <br>
-                    Your daily dose of <a href="http://www.wikipedia.pl">
-                    Wikipedia </a>
-                </p>
-                <img src="test_attachment.jpg">
-            </body>
-        </html>
-        """
-# turn these into plain/html MIMEText objects
-part2 = MIMEText(html,"html")
-
-# Add plain/html parts to MIMEMultipart message
-# The email client will try to render the last part first
-
-message.attach(part2)
-final_text=message.as_string()
+html = 'message.html'
 
 
-body = 'Hello!'
 filename='test_attachment.jpg'
 
 # keyring to be tested
-yag=yagmail.SMTP('dailyw.test@gmail.com')
+yag=yagmail.SMTP({'dailyw.test@gmail.com':'Daily Wiki'})
 
 yag.send(
     to=receiver_email,
-    subject='subject test',
-    contents=final_text,
-    attachments=filename,
+    subject='DailyW for '+today,
+    contents=[html],
 )
